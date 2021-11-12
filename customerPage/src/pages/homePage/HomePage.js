7ffb2f71f88ae1242aa2f12e5fd997623694d14d
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2021-11-12 22:54:02 +0700

    init

---
+++ customerPage/src/pages/homePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../../components/header/Header";
+
+/**
+ * @author
+ * @function HomePage
+ **/
+
+export const HomePage = (props) => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
