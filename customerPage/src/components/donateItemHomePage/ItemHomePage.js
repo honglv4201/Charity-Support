6bc0af42816112a516086ad96c507df5524b07af
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2021-11-13 23:25:43 +0700

    update json post

---
+++ customerPage/src/components/donateItemHomePage/ItemHomePage.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+/**
+ * @author
+ * @function ItemHomePage
+ **/
+
+export const ItemHomePage = ({ ...props }) => {
+  return (
+    <div className="donate-item">
+      <div className="image">
+        <img src={props.image} alt="" />
+      </div>
+      <div className="title">{props.title}</div>
+      <div className="desc">{props.desc}</div>
+      <div className="below-desc">
+        <div className="status-bar item2">
+          <span></span>
+        </div>
+        <div className="status">{props.time}</div>
+      </div>
+      <div className="donate-info">
+        <div className="target">
+          <div className="target__txt">Mục tiêu</div>
+          <div className="target__detail">
+            <span> {props.raised} </span>/ {props.goal}
+          </div>
+        </div>
+
+        <div className="btn-donate">
+          <button className="zoom-anim">Ủng hộ</button>
+        </div>
+      </div>
+    </div>
+  );
+};
